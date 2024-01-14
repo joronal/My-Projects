3f17952b341c26ba5cda8f3b61f9bb536d708dc0
joronal <djjoromix@gmail.com>
2024-01-15 00:55:24 +0200

    Create rename.py
    The script looks at the beginning of the file names (zip, 7z) for the string Sxxx, cuts this string and adds it to the end of the file name

---
+++ rename.py
@@ -0,0 +1,20 @@
+#The script looks at the beginning of the file names (zip, 7z) for the string Sxxx, cuts this string and adds it to the end of the file name
+import os
+import re
+current_dir = 'D:/Dropbox/'
+files = os.listdir(current_dir)
+for file in files:
+    if file.endswith('7z') or file.endswith('zip'):
+        match = re.match('S\d{4}', file)
+        if match:
+            fragment = match.group()
+            base, ext = os.path.splitext(file)
+            new_name = base.replace(fragment, '')
+            new_name = new_name.strip()
+            new_name = new_name + ' ' + fragment
+            new_name = new_name + ext
+            os.rename(os.path.join(current_dir, file), os.path.join(current_dir, new_name))
+            print(new_name)
+
+
+            
