324c6c5525bdcb8e756396c9bcf2c32bb0f1c042
joronal <djjoromix@gmail.com>
2024-01-15 01:24:15 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,7 +1,8 @@
 #The script looks at the beginning of the file names (zip, 7z) for the string Sxxx, cuts this string and adds it to the end of the file name
 import os
 import re
-current_dir = 'D:/Dropbox/'
+
+current_dir = 'DIR'
 files = os.listdir(current_dir)
 for file in files:
     if file.endswith('7z') or file.endswith('zip'):
